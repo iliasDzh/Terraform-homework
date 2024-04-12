c3f6ef5f0dde037de8c11fc6970d30bcae3be9e8
iliasDzh <Dzhakishev.ilias@gmail.com>
2024-04-12 16:22:26 +0000

    homework4 done

---
+++ homework4/README.md
@@ -0,0 +1,28 @@
+# Terraform-homework
+
+# Create virginia.tfvars and provide variables
+region = "us-east-1"
+ami_id = "ami-051f8a213df8bc089"
+type = "t2.micro"
+availability_zones = "us-east-1a"
+# Create ohio.tfvars and provide variables
+region = "us-east-2"
+ami_id = "ami-0900fe555666598a2"
+type = "t2.micro"
+availability_zones = "us-east-2a"
+# Create california.tfvars and provide variables
+region = "us-west-1"
+ami_id = "ami-0b990d3cfca306617"
+type = "t2.micro"
+availability_zones = "us-west-1a"
+# Create oregon.tfvars and provide variables
+region = "us-west-2"
+ami_id = "ami-0b990d3cfca306617"
+type = "t2.micro"
+availability_zones = "us-west-2a"
+
+
+
+
+
+
--- homework4/main.tf
+++ homework4/main.tf
@@ -1,48 +1,48 @@
 
-provider aws {
-     region = var.region
+provider "aws" {
+  region = var.region
 }
 
 
-  
+
 
 resource "aws_instance" "web" {
- ami = var.ami_id
- instance_type = "t2.micro"
- key_name  = var.key_name
- vpc_security_group_ids      = [aws_security_group.allow_tls.id]
- availability_zone = var.availability_zone
- count = var.instance_count
+  ami                    = var.ami_id
+  instance_type          = var.type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  availability_zone      = var.availability_zones
+  count                  = var.instance_count
 }
-variable ami_id {
+variable "ami_id" {
   default = ""
-  type = string
+  type    = string
 }
-variable type {
+variable "type" {
   default = ""
-  type = string
+  type    = string
 }
 
-variable region {
+variable "region" {
   default = ""
-  type = string
+  type    = string
 }
-variable key_name {
+variable "key_name" {
   default = ""
-  type = string
+  type    = string
 }
-variable availability_zone {
+variable "availability_zones" {
   default = ""
-  type = string
+  type    = string
 }
 
-variable instance_count {
-  default = ""
-  type = string
+variable "instance_count" {
+  default = 1
+  type    = number
 }
-variable ports {
-  default = "22,80,443"
-  type = list(number)
+variable "port" {
+  default = [22, 80, 443]
+  type    = list(number)
 }
 
 
